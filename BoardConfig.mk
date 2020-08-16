83d305cf2cb8b0e5b8aa3d4abbdc5687b9007e6f
Lakshay Garg <lakshaygarg.in@gmail.com>
2020-08-16 15:44:33 +0000

    beryllium: switch to LLVM tools

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@ TARGET_SCREEN_DENSITY := 440
 # Kernel
 TARGET_KERNEL_CONFIG := beryllium_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/beryllium
+KERNEL_SUPPORTS_LLVM_TOOLS := true
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
